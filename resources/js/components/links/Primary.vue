d3ce6243face72ff1b1c349daadf4ea85798f386
hcanas <harveyanas@gmail.com>
2021-05-22 12:41:30 +0800

    change underline to border bottom

--- resources/js/components/links/Primary.vue
+++ resources/js/components/links/Primary.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link class="text-red-700 hover:text-red-600 hover:underline focus:outline-none focus:text-red-600 focus:underline">
+    <router-link class="text-red-700 hover:text-red-600 border-b border-white hover:border-red-600 focus:outline-none focus:text-red-600 focus:border-red-600">
         <slot></slot>
     </router-link>
 </template>
