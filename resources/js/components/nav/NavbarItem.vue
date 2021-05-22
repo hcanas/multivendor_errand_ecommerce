0c5f58d947dd6dc68a3b536cf63b96527e85e59d
hcanas <harveyanas@gmail.com>
2021-05-22 12:40:52 +0800

    add static link option

--- resources/js/components/nav/NavbarItem.vue
+++ resources/js/components/nav/NavbarItem.vue
@@ -1,8 +1,18 @@
 <template>
-    <router-link :to="to" :class="`${active} font-bold hover:text-red-700 focus:text-red-700 focus:outline-none transition duration-200`">
+    <a v-if="is_static" href="#" @click.prevent :class="active">
         <div class="flex flex-col items-center space-x-1 rounded-l py-2 relative">
             <i :class="`${icon} text-xl w-6 lg:w-11 text-center`"></i>
-            <span class="hidden lg:inline text-xs">{{label}}</span>
+            <span class="hidden lg:inline text-xs">{{ label }}</span>
+            <span v-show="new_content" class="absolute w-3 h-3 rounded-full -top-1.5 -right-1.5">
+                <span class="animate-ping absolute top-0.5 right-0.5 w-3 h-3 bg-red-700 rounded-full opacity-75"></span>
+                <span class="absolute top-1 right-1 w-2 h-2 bg-red-700 rounded-full"></span>
+            </span>
+        </div>
+    </a>
+    <router-link v-else :to="to" :class="active">
+        <div class="flex flex-col items-center space-x-1 rounded-l py-2 relative">
+            <i :class="`${icon} text-xl w-6 lg:w-11 text-center`"></i>
+            <span class="hidden lg:inline text-xs">{{ label }}</span>
             <span v-show="new_content" class="absolute w-3 h-3 rounded-full -top-1.5 -right-1.5">
                 <span class="animate-ping absolute top-0.5 right-0.5 w-3 h-3 bg-red-700 rounded-full opacity-75"></span>
                 <span class="absolute top-1 right-1 w-2 h-2 bg-red-700 rounded-full"></span>
@@ -19,6 +29,7 @@
             icon: String,
             label: String,
             is_active: Boolean,
+            is_static: Boolean,
             new_content: Boolean,
         },
         computed: {
@@ -30,7 +41,15 @@
 </script>
 
 <style scoped>
+    a {
+        @apply font-bold hover:text-red-700 focus:text-red-700 focus:outline-none transition duration-200
+    }
+
     a.active {
         @apply text-red-700
+    }
+
+    a > div {
+       @apply flex flex-col items-center space-x-1 rounded-l py-2 relative
     }
 </style>
